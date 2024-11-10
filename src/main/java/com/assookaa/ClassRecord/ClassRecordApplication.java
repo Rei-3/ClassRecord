410285070a09acfebb365bc5088dd080d1f0a66c
Rei-3 <christan.endam@norsu.edu.ph>
2024-11-11 01:57:35 +0800

    Initial commit

---
+++ src/main/java/com/assookaa/ClassRecord/ClassRecordApplication.java
@@ -0,0 +1,13 @@
+package com.assookaa.ClassRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRecordApplication.class, args);
+	}
+
+}
